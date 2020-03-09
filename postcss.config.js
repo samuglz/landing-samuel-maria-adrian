f7a88b103f6bffdbd7aa4a9f95a98e11b340c9bf
Samuel Glez <samuelgonzalezalonso@gmail.com>
2020-03-09 13:47:02 +0000

    Cambiada configuracion de purgecss para que solo actue en el buildç

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,26 @@
-module.exports = {
-  plugins: {
-    tailwindcss: true,
-    "@fullhuman/postcss-purgecss": {
-      content: ["src/**/*.html"]
-    },
-    "postcss-preset-env": {
-      stage: 3,
-      features: {
-        "nesting-rules": true,
-        "custom-properties": true
-      }
-    },
-    "postcss-clean": true
-  }
+const isDev = process.env.NODE_ENV === 'dev';
+console.log(isDev);
+
+const config = {
+   plugins: {
+      tailwindcss: true,
+      '@fullhuman/postcss-purgecss': {
+         content: ['src/**/*.html'],
+         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      },
+      'postcss-preset-env': {
+         stage: 3,
+         features: {
+            'nesting-rules': true,
+            'custom-properties': true
+         }
+      },
+      'postcss-clean': true
+   }
 };
+
+if (isDev) delete config.plugins['@fullhuman/postcss-purgecss'];
+
+console.log(config);
+
+module.exports = config;
