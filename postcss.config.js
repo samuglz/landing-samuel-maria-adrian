1f4662c46553c57dd96310dc0d1accddb4aa55f0
Samuel Glez <samuelgonzalezalonso@gmail.com>
2020-04-21 14:10:20 +0100

    desactivado purgecss, porque eliminaba clases usadas

--- postcss.config.js
+++ postcss.config.js
@@ -3,21 +3,21 @@ const isDev = process.env.NODE_ENV === 'dev';
 const config = {
    plugins: {
       tailwindcss: true,
-      '@fullhuman/postcss-purgecss': {
-         content: ['src/**/*.html'],
-         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-      },
+      // '@fullhuman/postcss-purgecss': {
+      //    content: ['src/**/*.html'],
+      //    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      // },
       'postcss-preset-env': {
          stage: 3,
          features: {
             'nesting-rules': true,
-            'custom-properties': true
-         }
+            'custom-properties': true,
+         },
       },
-      'postcss-clean': true
-   }
+      'postcss-clean': true,
+   },
 };
 
-if (isDev) delete config.plugins['@fullhuman/postcss-purgecss'];
+// if (isDev) delete config.plugins['@fullhuman/postcss-purgecss'];
 
 module.exports = config;
