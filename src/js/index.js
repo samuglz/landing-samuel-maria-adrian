ac303711f7687aad66a2d9ccfee8b4346030555e
Samuel Glez <samuelgonzalezalonso@gmail.com>
2020-04-22 12:54:50 +0100

    Rediseñada la landing completa, se han creado secciones por separadas en vez del Carousel anterior, añadido logo y funcionalidad para cambiar el comportamiento del scroll-Y

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,7 @@
-const dots = document.querySelectorAll('.dot');
-let currentSlide = 1;
-const TIME_TO_SLIDE = 10000;
-const SM_SCREEN = 640;
-const FIRST_SLIDE = 1;
-const isDesktop = window.innerWidth > SM_SCREEN;
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
-const getNextSlide = (currentSlide) => currentSlide + 1;
-
-const updateDot = (dot) => {
-   dots.forEach((el) => el.classList.remove('active'));
-   dot.classList.add('active');
-   currentSlide = parseInt(dot.id.slice(4));
-};
-
-dots.forEach((dot) => {
-   dot.addEventListener('click', () => updateDot(dot));
-   dot.addEventListener('mousedown', () => clearInterval(autoplay));
+window.addEventListener('wheel', (e) => {
+   e.deltaY > 0 ? window.scrollBy(0, window.innerHeight) : window.scrollBy(0, -window.innerHeight);
 });
-
-const autoplay = setInterval(() => {
-   if (isDesktop) {
-      currentSlide = currentSlide === dots.length ? FIRST_SLIDE : getNextSlide(currentSlide);
-      const dotToGo = document.querySelector(`#nav-${currentSlide}`);
-      dotToGo.click();
-   }
-}, TIME_TO_SLIDE);
