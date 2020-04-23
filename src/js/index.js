5b6e0c2157be94e0d39b8ab267f6674aa3d78472
Samuel Glez <samuelgonzalezalonso@gmail.com>
2020-04-23 14:49:41 +0100

    eliminados los anclas de la parte inferior para navegar por las secciones, añadida una nueva seccion de navegacion

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,40 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 AOS.init();
 
+const dots = document.querySelectorAll('.dot');
+let stateSection = 1;
+
+function removeActiveClass() {
+   dots.forEach((el) => {
+      el.classList.remove('active');
+   });
+}
+
+function scrollDown() {
+   window.scrollBy(0, window.innerHeight);
+   if (stateSection < dots.length) {
+      stateSection++;
+   }
+}
+
+function scrollUp() {
+   if (stateSection > 1) {
+      stateSection--;
+   }
+   window.scrollBy(0, -window.innerHeight);
+}
+
+dots.forEach((dot) => {
+   dot.addEventListener('click', () => {
+      removeActiveClass();
+      dot.classList.add('active');
+      stateSection = parseInt(dot.id.slice(4));
+   });
+});
+
 window.addEventListener('wheel', (e) => {
-   e.deltaY > 0 ? window.scrollBy(0, window.innerHeight) : window.scrollBy(0, -window.innerHeight);
+   e.deltaY > 0 ? scrollDown() : scrollUp();
+   const activeDot = document.querySelector(`#nav-${stateSection}`);
+   removeActiveClass();
+   activeDot.classList.add('active');
 });
